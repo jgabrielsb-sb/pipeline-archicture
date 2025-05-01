13f4bff7fc6026c456b1d0fd1fcaee11d47c0601
Joao Gabriel <jgabrielsb2002@gmail.com>
2025-05-01 17:13:39 -0300

    feat: logger

---
+++ src/packag/modules/utils/logger.py
@@ -0,0 +1,17 @@
+import logging
+import logging.config
+import os
+from pathlib import Path
+from packag.modules.config import LOGGING_CONFIG
+
+LOG_DIR = Path(os.getenv('LOG_DIR'))
+os.makedirs(LOG_DIR, exist_ok=True)
+logging.config.dictConfig(LOGGING_CONFIG)
+
+def get_logger(name):
+    return logging.getLogger(name)
+
+
+
+
+
