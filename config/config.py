ed56ddb4dbf481febe4effb4d479b5f5848f3413
Joao Gabriel <jgabrielsb2002@gmail.com>
2025-05-02 10:32:26 -0300

    feat: task branch initial commit

--- config/config.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-from pathlib import Path
-
-load_dotenv()
-
-LOG_DIR = Path(os.getenv('LOG_DIR'))
--- src/modules/pipeline/task.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-class Task(ABC):
-    """
-    Abstract base class for all tasks.
-    """
-    
-    @abstractmethod
-    def run(self, previous_task_result=None):
-        pass
