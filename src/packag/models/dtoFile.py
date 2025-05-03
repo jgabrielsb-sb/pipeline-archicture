57bd9fe572c5268ff00331b941c1cd6676e724fa
Joao Gabriel <jgabrielsb2002@gmail.com>
2025-05-03 11:35:11 -0300

    feat: dto file model

---
+++ src/packag/models/dtoFile.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from pathlib import Path
+from enum import Enum
+
+
+class FileExtensionEnum(str, Enum):
+    PDF = "pdf"
+    DOCX = "docx"
+    DOC = "doc"
+    XLSX = "xlsx"
+    XLS = "xls"
+    CSV = "csv"
+
+class BaseFile(BaseModel):
+    """
+    This is the base class for all files.
+    It is used to validate the file path and the file extension.
+    """
+    file_path: Path
+    file_extension: FileExtensionEnum
+    
+    
+    
+
