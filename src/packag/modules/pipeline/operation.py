9eb141657215dcd9a9e7097d7d21429987c3f01f
Joao Gabriel <jgabrielsb2002@gmail.com>
2025-05-05 15:51:46 -0300

    doc: adding documenation on operation interface

--- src/packag/modules/pipeline/operation.py
+++ src/packag/modules/pipeline/operation.py
@@ -1,10 +1,18 @@
+from abc import ABC, abstractmethod
 
-class Operation:
+class Operation(ABC):
     """
     Abstract base class for all operations.
+    
+    An operation is a class that implements a specific task.
+    It has a run method that receives an input and returns an output.
+    
+    The operation, thus, does not perform any validation of the input or output.
+    This is done by the task that uses the operation.
     """
     
-    def run(self, input_data=None):
+    @abstractmethod
+    def run(self, input_data=None): # pragma: no cover
         """
         Run the operation.
         """
