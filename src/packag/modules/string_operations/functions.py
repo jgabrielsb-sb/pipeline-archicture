704dc6250edf7a098bd285c4545a97a87c6c9e41
Joao Gabriel <jgabrielsb2002@gmail.com>
2025-05-07 17:11:49 -0300

    feat: new string_operations module

---
+++ src/packag/modules/string_operations/functions.py
@@ -0,0 +1,169 @@
+import unicodedata
+
+from packag.modules.utils.decorators import log_execution
+
+from packag.modules.    utils.logger import get_logger
+
+string_operation_logger = get_logger('utils')
+
+@log_execution(string_operation_logger)
+def remove_all_accents(text: str) -> str:
+    """
+    Remove all accents from a string.
+    
+    Args:
+        text (str): The input string to remove accents from.
+        
+    Returns:
+        str: The input string without accents.
+        
+    Raises:
+        TypeError: If the input is not a string.
+        
+    """
+    if not isinstance(text, str):
+        string_operation_logger.error(f"Input must be a string")
+        raise TypeError("Input must be a string")
+    
+    return ''.join(char for char in unicodedata.normalize('NFD', text) if unicodedata.category(char) != 'Mn')
+
+@log_execution(string_operation_logger)
+def remove_all_spaces(text: str) -> str:
+    """
+    Remove all spaces from a string.
+    
+    Args:
+        text (str): The input string to remove spaces from.
+        
+    Returns:
+        str: The input string without spaces.
+        
+    Raises:
+        TypeError: If the input is not a string.
+        
+    """
+    if not isinstance(text, str):
+        string_operation_logger.error(f"Input must be a string")
+        raise TypeError("Input must be a string")
+    
+    return text.replace(' ', '')
+
+@log_execution(string_operation_logger)
+def get_upper_case_string(text: str) -> str:
+    """
+    Convert a string to uppercase.
+    
+    Args:
+        text (str): The input string to convert to uppercase.
+        
+    Returns:
+        str: The input string in uppercase.
+        
+    Raises:
+        TypeError: If the input is not a string.
+        
+    """
+    if not isinstance(text, str):
+        string_operation_logger.error(f"Input must be a string")
+        raise TypeError("Input must be a string")
+    
+    return text.upper()
+
+@log_execution(string_operation_logger)
+def get_str_with_only_numbers(text: str) -> str:
+    """
+    Get a string with only numbers.
+    
+    Args:
+        text (str): The input string to get only numbers from.
+        
+    Returns:
+        str: The input string with only numbers.
+        
+    Raises:
+        TypeError: If the input is not a string.
+        
+    """
+    if not isinstance(text, str):
+        string_operation_logger.error(f"Input must be a string")
+        raise TypeError("Input must be a string")
+    
+    return ''.join(char for char in text if char.isdigit())
+
+@log_execution(string_operation_logger)
+def get_formatted_cpf(cpf: str) -> str:
+    """
+    Formata um string para o formato de CPF: XXX.XXX.XXX-XX
+
+    :param cpf: string que representa o CPF
+    :return: cpf formatado XXX.XXX.XXX-XX
+    :raises ValueError: se a string não contém 11 dígitos.
+    """
+    
+    if not isinstance(cpf, str):
+        string_operation_logger.error(f"Input must be a string")
+        raise TypeError("Input must be a string")
+    
+    if len(cpf) != 11 or not cpf.isdigit():
+        string_operation_logger.error(f"CPF must be a string of exactly 11 digits.")
+        raise ValueError("CPF must be a string of exactly 11 digits.")
+    
+    formatted_cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+    return formatted_cpf
+
+@log_execution(string_operation_logger)
+def safe_float(value: object | None) -> float | None:
+    """
+    Converts value to float, returning None if conversion fails.
+    
+    Args:
+        value: The value to convert to float.
+        
+    Returns:
+        float: The value converted to float.
+        None: If the value is not a string or if conversion fails.
+    """
+    if value is None:
+        return None
+    
+    try:
+        return float(value)
+    except (TypeError, ValueError):
+        string_operation_logger.info(f"Could not convert {value} to float")
+        return None
+
+@log_execution(string_operation_logger)
+def safe_int(value: object | None) -> int | None:
+    """
+    Converts value to int, returning None if conversion fails.
+    """
+    if value is None:
+        return None
+    
+    try:
+        return int(value)
+    except (TypeError, ValueError):
+        string_operation_logger.info(f"Could not convert {value} to int")
+        return None
+
+
+@log_execution(string_operation_logger)
+def safe_str(value: object | None) -> str | None:
+    """
+    Converts value to str, returning None if conversion fails.
+    
+    Args:
+        value: The value to convert to str.
+        
+    Returns:
+        str: The value converted to str.
+        None: If the value is not a string or if conversion fails.
+    """
+    if value is None:
+        return None
+    
+    try:
+        return str(value)
+    except (TypeError, ValueError):
+        string_operation_logger.info(f"Could not convert to str")
+        return None
---
+++ src/packag/modules/string_operations/stringOperationsPipeline.py
@@ -0,0 +1,69 @@
+from .functions import *
+from abc import ABC, abstractmethod
+from typing import Callable
+
+from packag.utils.decorators import log_execution
+
+from packag.utils.logger import get_logger
+
+string_operations_logger = get_logger('string_operations')
+
+class StringOperationsPipeline(ABC):
+    
+    def __init__(self, pipeline: list[Callable]):
+        
+        if not isinstance(pipeline, list):
+            string_operations_logger.error(f"Input 'pipeline' must be a list")
+            raise TypeError("Input 'pipeline' must be a list")
+        
+        if not all(isinstance(function, Callable) for function in pipeline):
+            string_operations_logger.error(f"Input 'pipeline' must be a list of Callable functions")
+            raise TypeError("Input 'pipeline' must be a list of Callable functions")
+        
+        
+        self.pipeline = pipeline
+    
+    @log_execution(string_operations_logger)
+    def run(self, input_text: str):
+        """
+        Run the pipeline of string operations on the input text.
+        
+        Args:
+            text (str): The input text to process.
+            
+        Returns:
+            str: The processed text.
+            
+        Raises:
+            TypeError: If the input is not a string. 
+        """
+        
+        if not isinstance(input_text, str):
+            string_operations_logger.error(f"Input 'text' must be a string")
+            raise TypeError("Input 'text' must be a string")
+        
+        for function in self.pipeline:
+            input_text = function(input_text)
+            
+        if not isinstance(input_text, str):
+            string_operations_logger.error(f"Output must be a string")
+            raise TypeError("Output must be a string")
+            
+        return input_text
+
+
+NormalizeEnderecoPipeline = StringOperationsPipeline(
+    [
+        remove_all_accents,
+        remove_all_spaces,
+        get_upper_case_string
+    ]
+)
+
+NormalizeAtvEconomicaPipeline = StringOperationsPipeline(
+    [
+        get_str_with_only_numbers,
+        remove_all_spaces
+    ]
+)
+        
