50d36ce787bc16d6d74aa4a94c0a3abe630f310a
Joao Gabriel <jgabrielsb2002@gmail.com>
2025-05-08 09:51:10 -0300

    tests: new format data task tests

---
+++ src/tests/unit_tests/modules/pipeline/tasks/test_format_data_task.py
@@ -0,0 +1,95 @@
+import pytest
+
+from packag.modules.pipeline.tasks import FormatDataTask
+
+from packag.modules.pipeline.utils.exceptions import ValidationError
+
+from unittest.mock import MagicMock
+
+from pydantic import BaseModel
+
+from packag.modules.pipeline.utils.exceptions import TaskError
+
+from packag.modules.pipeline.operation import Operation
+
+from packag.models import dtoFile
+
+from pathlib import Path
+
+class TestFormatDataTask:
+    #
+    def test_if_private_validate_operation_cls_raise_type_error_when_operation_cls_is_not_a_operation(self):
+        with pytest.raises(TypeError):
+            FormatDataTask(operation_cls=None)._validate_operation_cls(operation_cls=None)
+            
+  
+    def test_if_private_validate_input_raise_type_error_when_input_is_not_a_dict(self):
+        with pytest.raises(TypeError):
+            FormatDataTask(operation_cls=None)._validate_input(input_data=None)
+        
+    
+    def test_if_private_validate_input_raise_type_error_when_it_is_not_a_dict_and_is_not_none(self):
+        with pytest.raises(TypeError):
+            FormatDataTask(operation_cls=None)._validate_input(input_data=str)
+        
+    
+    def test_if_private_validate_input_return_the_input_when_it_is_a_dict(self):
+        
+        dummy_dict = {
+            'key1': 'value1',
+            'key2': 'value2'
+        }
+        
+        input_data = dummy_dict
+        assert FormatDataTask(operation_cls=None)._validate_input(input_data=input_data) == input_data
+
+    #
+    def test_if_private_validate_output_raise_type_error_when_output_is_not_a_dict(self):
+        with pytest.raises(TypeError):
+            FormatDataTask(operation_cls=None)._validate_output(output_data=None)
+            
+    def test_if_private_validate_output_raise_type_error_when_output_is_not_a_dict_and_is_not_none(self):
+        with pytest.raises(TypeError):
+            FormatDataTask(operation_cls=None)._validate_output(output_data=str)
+            
+    def test_if_private_validate_output_return_the_output_when_it_is_a_dict(self):
+        
+        dummy_dict = {
+            'key1': 'value1',
+            'key2': 'value2'
+        }
+        
+        class MockPydanticBaseModel(MagicMock, BaseModel):
+            pass
+        
+        output_data = dummy_dict
+        assert FormatDataTask(operation_cls=None)._validate_output(output_data=output_data) == output_data
+            
+    def test_if_private_validate_operation_cls_raise_type_error_when_operation_cls_is_not_a_subclass_of_operation(self):
+        with pytest.raises(TypeError):
+            operation = str
+            FormatDataTask(operation_cls=operation)._validate_operation_cls(operation_cls=operation)
+            
+    #
+    def test_if_validate_operation_cls_return_the_operation_when_it_is_valid(self):
+        class MockOperation(MagicMock, Operation):
+            def run(self, input_data=None):
+                return None
+            
+        operation = MockOperation
+        assert FormatDataTask(operation_cls=operation).validate_operation_cls(operation_cls=operation) == operation
+            
+    def test_if_validate_operation_cls_raise_validation_error_when_operation_cls_is_not_a_subclass_of_operation(self):
+        with pytest.raises(ValidationError):
+            FormatDataTask(operation_cls=None).validate_operation_cls(operation_cls=None)
+    
+    def test_if_validate_input_raise_validation_error_when_input_is_not_a_dict(self):
+        with pytest.raises(ValidationError):
+            FormatDataTask(operation_cls=None).validate_input(input_data=str)
+        
+    def test_if_validate_output_raise_validation_error_when_output_is_not_a_dict(self):
+        with pytest.raises(ValidationError):
+            FormatDataTask(operation_cls=None).validate_output(output_data=None)
+        
+    
+
