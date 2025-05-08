8f6d14fdebf0829f70a315b31698ebf9b5ec161a
Joao Gabriel <jgabrielsb2002@gmail.com>
2025-05-08 16:37:32 -0300

    feat: new model on dtoNota

--- src/packag/models/business/dtoNota.py
+++ src/packag/models/business/dtoNota.py
@@ -14,6 +14,7 @@ from .enum import (
 
 class NotaExtractedInfo(BaseModel):
     
+    
     numero_nfs: str # numero
     codigo_autenticidade: str
     
@@ -48,6 +49,63 @@ class NotaExtractedInfo(BaseModel):
     
 class NotaFormattedInfo(BaseModel):
     
+    numero_nfs: str
+    codigo_autenticidade: str
+    
+    data_competencia: str
+    
+    valor_liquido: str
+    valor_total: str 
+    valor_deducoes: str 
+    valor_pis: str
+    valor_cofins: str
+    valor_inss: str
+    valor_irrf: str
+    valor_csll: str
+    valor_issqn: str
+    base_calculo: str
+    aliquota: str
+    issqn_a_reter: str
+    
+    estado: str
+    
+    codigo_tributacao: str 
+    discriminacao_servico: str
+    opt_simples_nacional: str
+    
+    serie: Optional[str] = None    
+    nfse_substituida: Optional[str] = None 
+    valor_outras_retencoes: Optional[str] = None
+    data_emissao: Optional[str] = None
+    
+    atv_economica: str
+    atv_economica_normalized: str
+    municipio: str
+    
+    @model_validator(mode='after')   # must have only numbers in the following fields: cnpj, cpf, inscricao_municipal, cep, telefone
+    def only_numbers(cls, values):
+        only_numbers_fields = [
+            'codigo_tributacao',
+            'atv_economica_normalized',
+            'numero_nfs',
+            'opt_simples_nacional'
+        ]
+        data_as_dict = values.model_dump()
+        fields_that_doesnt_passed_the_condition = []
+        
+        for field in only_numbers_fields:
+            for char in data_as_dict[field]:
+                if not char.isdigit():
+                    fields_that_doesnt_passed_the_condition.append(field)
+                    break
+    
+        if fields_that_doesnt_passed_the_condition:
+            raise ValueError(f"The following fields must contain only numbers: {fields_that_doesnt_passed_the_condition}")
+        
+        return values
+    
+class NotaConvertedInfo(BaseModel):
+    
     numero_nfs: int
     codigo_autenticidade: str
     
@@ -81,25 +139,7 @@ class NotaFormattedInfo(BaseModel):
     atv_economica_normalized: str
     municipio: MunicipioEnum
     
-    @model_validator(mode='after')   # must have only numbers in the following fields: cnpj, cpf, inscricao_municipal, cep, telefone
-    def only_numbers(cls, values):
-        only_numbers_fields = [
-            'codigo_tributacao',
-            'atv_economica_normalized'
-        ]
-        data_as_dict = values.model_dump()
-        fields_that_doesnt_passed_the_condition = []
-        
-        for field in only_numbers_fields:
-            for char in data_as_dict[field]:
-                if not char.isdigit():
-                    fields_that_doesnt_passed_the_condition.append(field)
-                    break
     
-        if fields_that_doesnt_passed_the_condition:
-            raise ValueError(f"The following fields must contain only numbers: {fields_that_doesnt_passed_the_condition}")
-        
-        return values
     
     
     
